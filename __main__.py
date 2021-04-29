ed55411d622955f603e533d043f9776b2a014085
Arslan160572 <36481319+Arslan160572@users.noreply.github.com>
2021-04-29 13:41:52 +0500

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -435,7 +435,7 @@ def configure_sshd():
      #   'KexAlgorithms': 'curve25519-sha256,curve25519-sha256@libssh.org,diffie-hellmangroup14-sha256,diffie-hellman-group16-sha512,diffie-hellman-group18-sha512,ecdh-sha2-nistp521,ecdh-sha2-nistp384,ecdh-sha2-nistp256,diffiehellman-group-exchange-sha256',
         'ClientAliveInterval': '300',
         'ClientAliveCountMax': '0',
-        'LoginGraceTime': '60',
+        'LoginGraceTime': '120',
         'AllowUsers': 'ec2-user',
         'Banner': '/etc/issue.net'
     }).write()
